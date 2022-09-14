149e425e1627117cf179eec40c28a4c7f2ea9bcc
Bruno Santos <bruno.cabdsantos@gmail.com>
2022-09-14 19:36:42 -0300

    matrix rotation

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -2,9 +2,9 @@
 import { Codemirror } from "vue-codemirror";
 import { javascript } from "@codemirror/lang-javascript";
 import { oneDark } from "@codemirror/theme-one-dark";
-import { ref, shallowRef } from "vue";
+import { ref, shallowRef, unref } from "vue";
 import { getPixels, savePixels } from "ndarray-pixels";
-import type ndarray from "ndarray";
+import ndarray from "ndarray";
 const code = ref(`for (let i = 0; i < pixels.value.shape[0]; ++i) {
   for (let j = 0; j < pixels.value.shape[1]; ++j) {
     pixels.value.set(i, j, 1, 255);
@@ -29,19 +29,50 @@ const uploadedBytes = ref<Uint8Array | null>(null);
 const pixels = ref<ndarray.NdArray<
   ndarray.TypedArray | ndarray.GenericArray<number> | number[]
 > | null>(null);
+var alteredPixels = ref< ndarray.NdArray<
+  ndarray.TypedArray | ndarray.GenericArray<number> | number[]
+> | null>(null);
 const transformedBytes = ref<Uint8Array | null>(null);
+
+
+
+// function transpose(matrix) {
+//   return matrix[0].map((col, i) => matrix.map(row => row[i]));
+// }
 
 function readUploadedImagePixels(file: File) {
   file.arrayBuffer().then(async (buffer) => {
     uploadedBytes.value = new Uint8Array(buffer);
     pixels.value = await getPixels(uploadedBytes.value, "image/png");
-    for (let i = 0; i < pixels.value.shape[0]; ++i) {
-      for (let j = 0; j < pixels.value.shape[1]; ++j) {
-        pixels.value.set(i, j, 1, 255);
-      }
-    }
+    // for (let i = 0; i < pixels.value.shape[0]; ++i) {
+    //   for (let j = 0; j < pixels.value.shape[1]; ++j) {
+    //     pixels.value.set(i, j, 1, 255);
+    //     pixels.value.data
+    //   }
+    // }
+
+    // var newArray = [];
+    // for(var i = 0; i < array.length; i++){
+    //     newArray.push([]);
+    // };
+
+    // for(var i = 0; i < array.length; i++){
+    //     for(var j = 0; j < arrayLength; j++){
+    //         newArray[j].push(array[i][j]);
+    //     };
+    // };
+
+//pixels.value.get()
+    log(pixels.value);
+
+    alteredPixels.value = ndarray(
+      new Uint8Array(pixels.value.data),
+      unref(pixels.value.shape),
+      unref(pixels.value.stride),
+      unref(pixels.value.offset)
+    );
     // write
-    transformedBytes.value = await savePixels(pixels.value, "image/png");
+    transformedBytes.value = await savePixels(alteredPixels.value!, "image/png");
     outputSrc.value = URL.createObjectURL(
       new Blob([transformedBytes.value], { type: "image/png" } /* (1) */)
     );
@@ -57,23 +88,24 @@ function previewImage() {
   }
 }
 
-function apply() {
+
+async function apply() {
   // modify
   if (pixels.value) {
-    // try{
-    // }
-    // catch(e){
-    // }
-    // for (let i = 0; i < pixels.value.shape[0]; ++i) {
-    //   for (let j = 0; j < pixels.value.shape[1]; ++j) {
-    //     pixels.value.set(i, j, 1, 255);
-    //   }
-    // }
-    // write
-    // transformedBytes.value = await savePixels(pixels.value, "image/png");
-    // outputSrc.value = URL.createObjectURL(
-    //   new Blob([transformedBytes.value], { type: "image/png" } /* (1) */)
-    // );
+    alteredPixels.value = ndarray(
+      new Uint8Array(pixels.value.data),
+      unref(pixels.value.shape),
+      unref(pixels.value.stride),
+      unref(pixels.value.offset)
+    );
+    window.eval.call(window,'(function (pixels) {'+code.value+'})')(alteredPixels);
+
+    
+  
+    transformedBytes.value = await savePixels(alteredPixels.value!, "image/png");
+    outputSrc.value = URL.createObjectURL(
+      new Blob([transformedBytes.value], { type: "image/png" } /* (1) */)
+    );
   }
 }
 </script>
@@ -93,7 +125,7 @@ function apply() {
         <v-img
           :src="previewSrc"
           max-height="500"
-          cover
+          contain
           class="bg-grey-lighten-2"
         ></v-img>
       </div>
@@ -111,7 +143,7 @@ function apply() {
           @focus="log('focus', $event)"
           @blur="log('blur', $event)"
         />
-        <v-btn class="apply-button">Aplicar</v-btn>
+        <v-btn class="apply-button" @click="apply">Aplicar</v-btn>
         <!-- <button @click="apply" class="apply-button">Aplicar</button> -->
       </div>
     </div>
@@ -120,7 +152,7 @@ function apply() {
       <v-img
         :src="outputSrc"
         max-height="500"
-        cover
+        contain
         class="bg-grey-lighten-2"
       ></v-img>
     </div>
